e828920982ab7f99a440be4e4beb92dc789c8a1f
Umedjon Saidulloev <umedjon.saidulloev@mail.ru>
2024-03-20 10:03:00 +0500

    Lesson1

--- app/src/main/java/com/example/shoplistprojectasosi/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplistprojectasosi/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shoplistprojectasosi
+package com.example.shoplistprojectasosi.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoplistprojectasosi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
